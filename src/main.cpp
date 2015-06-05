fccb342c112ea8e9870deb7e554d9e840ca192eb
Viktor Anjin <viktor.anjin@gmail.com>
2015-06-04 22:27:52 -0700

    add plugins directory to default search path

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 int main(int argc, char **argv)
 {
 	gst_init (&argc, &argv);
+
+	GstRegistry *registry;
+	registry = gst_registry_get();
+	gst_registry_scan_path(registry, "./plugins");
+
 	QApplication app(argc, argv);
 
 	MainWindow wgt;
